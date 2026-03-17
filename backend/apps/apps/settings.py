fd1f33da2dea34644d912f53a263600fb0e8e752
Rensytimsy <rensytimsy03@gmail.com>
2026-03-17 18:51:24 +0300

    none

--- backend/apps/apps/settings.py
+++ backend/apps/apps/settings.py
@@ -15,18 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-9v_h=#lhc*4_b7@!%ofm+=e_fwh7cbg)9f_(b(8=is7$3&2e+o'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "localhost:3000",
+    "localhost",
     "evoque-space.onrender.com"
 ]
 
