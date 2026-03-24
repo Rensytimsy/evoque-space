fbe1809a8ac2959ad3523bbf5bc5f0b3e4ba9ea0
Rensytimsy <rensytimsy03@gmail.com>
2026-03-24 09:43:51 +0300

    minor changes

--- frontend/app/pages/test/page.tsx
+++ frontend/app/pages/test/page.tsx
@@ -1,15 +1,46 @@
 "use client"
+import { Products } from "@/lib/datafetch"
+import { getProducts } from "@/lib/datafetch"
+import { GetStaticProps } from "next"
+import { FC } from "react"
+import { unstable_cache } from "next/cache"
 
-import React from 'react';
-import { ShoppingCart, ArrowRight, ChevronLeft, ChevronRight, Zap, Shield, Cpu } from 'lucide-react';
-import Link from 'next/link';
+export const dynamic = "force-static";
+export const regenerate = 300;
 
-const SolarHero = () => {
-  return (
-    <div>
-      hello this is the test page
-    </div>
-  )
-};
+const cacheAllProducts = unstable_cache(
+    async () => {
+        const productsdata = await getProducts();
+        return productsdata;
+    },
+    ['products'],
+    {
+        revalidate: regenerate,
+        tags: ['products']
+    }
+)
 
-export default  SolarHero;
+
+
+const TestPage:FC<{ productsdata : Products }> = async() => {
+    const products = await cacheAllProducts();
+    if (!products || products.length < 1){
+        return <div>No products were found</div>
+    }
+    return(
+        <div className="flex justify-cente align-center h-full">
+            <div className="grid grid-cols-6">
+                {products.map((p, i) => (
+                    <div className="">
+                        <div>
+                            <img src={p.image} alt="product image" />
+                        </div>
+                        {p.title}
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default TestPage;
